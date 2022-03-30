6bded8ec8e221056f031d0efab78453a76ae4ec1
朱理 <2053095395@qq.com>
2022-03-30 11:03:31 +0800

    3-30

--- src/main/java/com/zhuli/file/model/VersionModel.java
+++ src/main/java/com/zhuli/file/model/VersionModel.java
@@ -2,9 +2,14 @@ package com.zhuli.file.model;
 
 import com.zhuli.file.request.FileController;
 import lombok.Data;
+import sun.rmi.runtime.Log;
 
 import java.io.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
 import java.util.Properties;
 
 /**
@@ -31,9 +36,10 @@ public class VersionModel {
             prop.load(in);
             version = prop.getProperty("app.version");
             type = Integer.parseInt(prop.getProperty("app.type"));
-            url = prop.getProperty("app.url");
+//            url = getLocalHostLANAddress().getHostAddress() + prop.getProperty("app.url");
+            url = "http:" + getLocalHostLANAddress().getHostAddress() + ":8088/api/file/download";
             content = prop.getProperty("app.content");
-
+            
         } catch (IOException e) {
             System.err.println("加载项目同级目录下的version.properties文件失败，请检查是否存在文件，文件内容为：\n{" +
                     "# 版本号\n" +
@@ -41,10 +47,49 @@ public class VersionModel {
                     "# 类型\n" +
                     "app.type=3\n" +
                     "# 下载地址\n" +
-                    "app.url=http:127.0.0.1:8088/api/file/download\n" +
+                    "app.url=http:/api/file/download\n" +
                     "# 更新说明\n" +
                     "app.content=apk版本更新}");
             e.printStackTrace();
+        }
+    }
+
+
+    // 正确的IP拿法，即优先拿site-local地址
+    private static InetAddress getLocalHostLANAddress() throws UnknownHostException {
+        try {
+            InetAddress candidateAddress = null;
+            // 遍历所有的网络接口
+            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements(); ) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+                // 在所有的接口下再遍历IP
+                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements(); ) {
+                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                    if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了
+                            return inetAddr;
+                        } else if (candidateAddress == null) {
+                            // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress;
+            }
+            // 如果没有发现 non-loopback地址.只能用最次选的方案
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            if (jdkSuppliedAddress == null) {
+                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+            }
+            return jdkSuppliedAddress;
+        } catch (Exception e) {
+            UnknownHostException unknownHostException = new UnknownHostException(
+                    "Failed to determine LAN address: " + e);
+            unknownHostException.initCause(e);
+            throw unknownHostException;
         }
     }
 
